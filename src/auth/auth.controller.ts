69fb01a00df4156c411a8268763694acb60fd924
moise_alexandru <moise.alexandru.ichb@gmail.com>
2023-04-16 04:14:35 +0300

    adaugat logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guard/local-auth.guard';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
 
 @Controller('')
 export class AuthController {
     constructor(
         private authService: AuthService
     ) {}
+
     @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Req() req, @Res({passthrough: true}) res: Response) {
@@ -19,4 +21,11 @@ export class AuthController {
         };
     }
 
+    @Post('logout')
+    async logout(@Res({passthrough: true}) res: Response) {
+        res.clearCookie('auth-token');
+        return {
+          message: 'Logged out successfully',
+        };
+    }
 }
